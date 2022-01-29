82531c4e2f29597ddd841f696b44177290f67aca
Alexander Lobachev <ss_di@mail.ru>
2022-01-29 14:44:33 +0300

    ege 15

---
+++ 14 - 29.01.2022 - ЕГЭ 5/p1.pas
@@ -0,0 +1,28 @@
+﻿function avt(x: integer): integer;
+var t, s, d, e, s1, s2, min, max: integer;
+begin
+    t := x div 1000;
+    s := x div 100 mod 10;
+    d := x div 10 mod 10;
+    e := x mod 10;
+    s1 := t + s;
+    s2 := d + e;
+    if s1 > s2 then begin
+        min := s2;
+        max := s1;
+    end else begin
+        min := s1;
+        max := s2;
+    end;
+    avt := max * 100 + min;
+end;
+
+var i, res: integer;
+begin
+   for i := 9999 downto 1000 do
+      if avt(i) = 1311 then begin
+          writeln(i);
+          res := i;
+      end;
+   writeln('res = ', res)
+end.
---
+++ 14 - 29.01.2022 - ЕГЭ 5/p2.pas
@@ -0,0 +1,36 @@
+﻿function avt(x: integer): integer;
+var x2, y, i, st: integer;
+    s : string;
+begin
+    x2 := x;
+    s := '';
+    while (x > 0) do begin
+        s := x mod 2 + s;
+        x := x div 2;
+    end;
+    delete(s, 1, 1);
+    y := 0;
+    st := 1;
+    for i := length(s) downto 1 do begin
+        if s[i] = '1' then
+           y := y + st;
+        st := st * 2;
+    end;
+    avt := x2 - y;
+end;
+
+var i, kol: integer;
+a : array[1..10000] of integer;
+begin
+   for i := 1 to 10000 do a[i] := -1;
+   
+   kol := 0;
+   for i := 500 to 5000 do begin
+       if not (avt(i) in a) then begin
+           kol := kol + 1;
+           a[kol] := avt(i);
+       end;
+//       writeln(i, ' ', avt(i));
+   end;
+   writeln(a);
+end.
