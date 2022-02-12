aacef0bdee14fdf3e99993539b91df8f8dac2498
Alexander Lobachev <ss_di@mail.ru>
2022-02-12 15:19:48 +0300

    егэ 15 часть 2

---
+++ 16 - 12.02.2022 - ЕГЭ 15 часть 2/p-33-v2.pas
@@ -0,0 +1,21 @@
+﻿function f(x, y:real; a:integer): boolean;
+begin
+    f := (y-x <> 5) or 
+         (a < 2*x*x*x + y) or
+         (a < y*y + 16);
+end;
+
+var
+   x, y, a: integer;
+   ok: boolean;
+begin
+    for a := 39 to 50 do begin
+        ok := true;
+        for x := 1 to 10000 do
+            for y := 1 to 10000 do
+                if not f(x/1000, y/1000, a) then
+                    ok := false;
+        if ok then
+            writeln(a);
+    end;
+end.
---
+++ 16 - 12.02.2022 - ЕГЭ 15 часть 2/p-33.pas
@@ -0,0 +1,21 @@
+﻿function f(x, y, a:integer): boolean;
+begin
+    f := (y-x <> 5) or 
+         (a < 2*x*x*x + y) or
+         (a < y*y + 16);
+end;
+
+var
+   x, y, a: integer;
+   ok: boolean;
+begin
+    for a := 0 to 1000 do begin
+        ok := true;
+        for x := 1 to 1000 do
+            for y := 1 to 1000 do
+                if not f(x, y, a) then
+                    ok := false;
+        if ok then
+            writeln(a);
+    end;
+end.
