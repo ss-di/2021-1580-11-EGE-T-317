206fcde204bbe36a3bada38f50fe0b7b0911a669
Alexander Lobachev <ss_di@mail.ru>
2022-02-26 15:28:42 +0300

    разбор 24 и 27 с 19.02

---
+++ 17 - 26.02.2022 - Разбор 19.02/p24-n.pas
@@ -0,0 +1,35 @@
+﻿var
+    f: text;
+    s: string;
+    cur_l, max_l, i:integer;
+begin
+    assign(f, '24.txt');
+    reset(f);
+    read(f, s);
+    writeln(length(s));
+    max_l := -1;
+    i := 1;
+    cur_l:=0;
+    while i < length(s) do begin
+        if (s[i] = 'A') and ((s[i+1] = 'B') or (s[i+1] = 'C')) then
+            cur_l := cur_l + 1
+        else
+            cur_l := 0;
+        if cur_l > max_l then
+            max_l := cur_l;
+        i := i + 2;
+    end;
+    i := 2;
+    cur_l:=0;
+    while i < length(s) do begin
+        if (s[i] = 'A') and ((s[i+1] = 'B') or (s[i+1] = 'C')) then
+            cur_l := cur_l + 1
+        else
+            cur_l := 0;
+        if cur_l > max_l then
+            max_l := cur_l;
+        i := i + 2;
+    end;
+
+    writeln(max_l);
+end.
---
+++ 17 - 26.02.2022 - Разбор 19.02/p27-n.pas
@@ -0,0 +1,42 @@
+﻿var
+    f: text;
+    n, i, b, e, ml, ost: integer;
+    sum, ms: int64;
+    a: array[1.. 2561740] of integer;
+    smin, smax: array[0..66] of int64;
+begin
+    assign(f, '27_b.txt');
+    reset(f);
+    read(f, n);
+    writeln(n);
+    for i := 1 to n do
+        read(f, a[i]);
+    close(f);
+    for i := 1 to 66 do begin
+        smin[i] := -1;
+        smax[i] := -1;
+    end;
+    {2147483647
+    208 957 389
+    100000000000
+    9223372036854775807}
+    smin[0] := 0;
+    smax[0] := 0;
+    sum := 0;
+    for i := 1 to n do begin
+        sum := sum + a[i];
+        ost := sum mod 67;
+        if smin[ost] = -1 then begin
+            smin[ost] := sum;
+            smax[ost] := sum;
+        end else
+            smax[ost] := sum;
+    end;
+    
+    ms := -1;
+    for i := 0 to 66 do
+        if smax[i] - smin[i] > ms then
+            ms := smax[i] - smin[i];
+
+    writeln(ms);
+end.
